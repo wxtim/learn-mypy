8fce9f858069800193875e1bd96259baf71598c1
Edmund Henley <45294015+edmundhenley-mo@users.noreply.github.com>
2021-04-27 10:17:01 +0100

    Tweak more complex examples for clarity
    A few tweaks to support self-learning of this material (i.e. without
    someone talking through, adding verbal explanations on tricky bits)
    
    * Moved the Any example to end, as best not to start with this
      * Added comment to show more explicitly what Any can cover
        * Intentionally excluded classes - too hard! But covered by ...!
      * Added filled List, and comment on what then gets caught by mypy
        * linked in comment for Dict
      * added clarifying comments for Optional and Union
      * Added linebreak to distinguish scope of But and Normally sentences

--- 01.basic_syntax.py
+++ 01.basic_syntax.py
@@ -6,15 +6,16 @@ bar: int = 42
 
 # define something more complex (Python 3.6-3.8)
 from typing import Dict, Optional, Union, Any, List, Iterable
-baz: Any = 'Anything really'
 mylist: List = []
-mydict: Dict = {}
+my_nailed_down_list: List[str] = ["a", "b"]  # mypy will flag = [1, 2]
+mydict: Dict = {}  # can nail down in similar fashion
+myunsure: Optional[str] = "Hello, I could also be None"  # i.e. = None also OK
+mychoice: Union[str, int] = 42  # Equally valid: = "meep"
+baz: Any = "Anything really"  # Equally valid: = int; = None; ...
 myiterable: Iterable = range(12)
-myunsure: Optional[str] = 'Hello'
-mychoice: Union[str, int] = 42
-
 
 # But MyPy can usually work types like this out.
+
 # Normally you'd start typing at function or Class level:
 
 # `->` Arrow syntax shows what is expected:
